6d5f5612bceb013283dc118d32bf8b0bab19f820
nolan-mortier <nolan.mortier@student.ehb.be>
2024-10-02 11:24:56 +0200

    spaceinvader oef 1

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,30 @@
 "use strict";
 
-alert("Draw your space invader here");
+drawRectancle();
+
+function drawRectancle() {
+
+   let canvas = document.querySelector("canvas");
+   canvas.width = window.innerWidth;
+   canvas.height = window.innerHeight;
+   let context = canvas.getContext("2d");
+   
+   
+   context.fillStyle = "black";
+   context.beginPath();
+   context.rect(25,10, 300, 300);
+   context.fill();
+
+   
+   context.fillStyle = "#39FF14";
+   context.beginPath();
+   context.rect(50,150, 50, 50);
+   context.rect(150,150, 50, 50);
+   context.rect(250,150, 50, 50);
+   context.rect(200,100, 50, 50);
+   context.rect(100,100, 50, 50);
+   context.rect(100,50, 150, 50);
+   context.fill();
+
+
+}
