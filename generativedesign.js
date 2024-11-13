f6ada0be58e4b10560918327438da76ce37d9ace
nolan-mortier <nolan.mortier@student.ehb.be>
2024-11-13 16:29:51 +0100

    generative design oefening : vierkantjes in de afbeelding gekregen. Veranderen ook steeds van kleur. Niet de meest efficiente manier ga daar nu oplossingen voor zoeken. Ook mijn window.resize proberen te laten werken

--- generativedesign.js
+++ generativedesign.js
@@ -2,8 +2,11 @@
 
 /** @type {CanvasRenderingContext2D} */
 
+
 let context;
 setup();
+let x = 100;
+let y = 100;
 
 
 function setup() {
@@ -11,25 +14,30 @@ let canvas = document.querySelector("canvas");
    canvas.width = window.innerWidth;
    canvas.height = window.innerHeight;
  context = canvas.getContext("2d");
-drawLine();
+drawSquares();
 }
 
-   function drawLine() {
-    drawColor(500,20);
-    drawColor(800,40);
-    drawColor(500,30);
-    drawColor(100,25);
-    drawColor(700,45);
-
-   }
-
-
-
-   function drawColor(position,size){
+   function drawSquares() {
     let red = Math.random()* 255;
     let green = Math.random()* 255;
     let blue = Math.random()* 255;
-
 context.fillStyle = "rgb(" + red + "," + green + "," + blue +")";
-context.fillRect(position,position,size,size);
+context.fillRect(915,130,30,30);
+context.fillRect(945,100,30,30);
+context.fillRect(975,130,30,30);
+context.fillRect(885,160,30,30);
+context.fillRect(1005,160,30,30);
+context.fillRect(855,190,30,30);
+context.fillRect(1035,190,30,30);
+context.fillRect(825,220,30,30);
+context.fillRect(1065,220,30,30);
+context.fillRect(855,250,30,30);
+context.fillRect(885,280,30,30);
+context.fillRect(915,310,30,30);
+context.fillRect(945,280,30,30);
+context.fillRect(975,310,30,30);
+context.fillRect(1005,280,30,30);
+context.fillRect(1035,250,30,30);
+context.fillRect(850,110,30,30);
+context.fillRect(1035,110,30,30);
    }
